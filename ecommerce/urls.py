5548f5f8a328e33a3b8d93ca2f9d376360e1b4bc
007vict <super_vg@bk.ru>
2020-05-02 13:17:38 +0300

    create ecommerce

---
+++ ecommerce/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import home_page, about_page, contact_page, login_page
+
+urlpatterns = [
+    path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
+    path('login/', login_page),
+    path('admin/', admin.site.urls),
+]
