e3391ad65a682db17eabab9e84e49e5493dca43a
007vict <super_vg@bk.ru>
2020-05-04 15:13:08 +0300

    django admin

--- products/models.py
+++ products/models.py
@@ -4,3 +4,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     descriptions = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+
+    def __str__(self):
+        return self.title
