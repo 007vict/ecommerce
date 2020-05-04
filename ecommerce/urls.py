4b52f5210cabacfbc01064c2b97d1e3bc8607aa1
007vict <super_vg@bk.ru>
2020-05-04 17:00:14 +0300

    DetailView

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListview, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -14,13 +14,13 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
-    path('products/', ProductListview.as_view()),
+    path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
+    path('products/<pk>/', ProductDetailView.as_view()),
+    path('products-fbv/<pk>/', product_detail_view),
     path('admin/', admin.site.urls),
-    ]
+]
 
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
