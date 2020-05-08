c9dc3224c01279050e69b587bb789bfdeec52152
007vict <super_vg@bk.ru>
2020-05-08 13:34:13 +0300

    custom model manager, featured custom queryset

--- products/models.py
+++ products/models.py
@@ -17,18 +17,37 @@ def upload_image_path(instance, filename):
         new_filename=new_filename,
         final_filename=final_filename)
 
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+    def featured(self):
+        return self.filter(featured=True, active=True)
+
 class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self):
+        return self.get_queryset().active()
+
+    def featured(self):
+        return self.get_queryset().featured()
+
     def get_by_id(self, id):
-        qs = self.get_queryset.filter(id=id)
+        qs = self.get_queryset().filter(id=id)
         if qs.count() == 1:
             return qs.first()
         return None
+
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     descriptions = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    featured = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
     objects = ProductManager()
 
--- products/views.py
+++ products/views.py
@@ -4,14 +4,32 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     return Product.objects.all()
+
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListview, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 def product_list_view(request):
     querset = Product.objects.all()
@@ -21,7 +39,7 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -29,6 +47,19 @@ class ProductDetailView(DetailView):
         print(context)
         # context["abc"] = 123
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get("pk")
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get("pk")
+    #     return Product.objects.filter(pk=pk)
 
 def product_detail_view(request, pk=None, *args, **kwargs):
     # instance = Product.objects.get(pk=pk)
@@ -43,12 +74,17 @@ def product_detail_view(request, pk=None, *args, **kwargs):
     # except:
     #     print("huh?")
 
-    qs = Product.objects.filter(id=pk)
-    print(qs)
-    if qs.exists() and qs.count() == 1:
-        instance = qs.first()
-    else:
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
         raise Http404("Product doesn't exist")
+    # print(instance)
+    #
+    # qs = Product.objects.filter(id=pk)
+    # print(qs)
+    # if qs.exists() and qs.count() == 1:
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product doesn't exist")
 
     context = {
         "object": instance,
