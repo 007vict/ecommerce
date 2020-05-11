95fda04ae307d485bb02c942665914d433a3527e
007vict <super_vg@bk.ru>
2020-05-11 18:31:18 +0300

    create Cart model

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #our products
+    'carts',
     'products',
     'search',
     'tags',
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -5,15 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-# from products.views import (
-#     ProductListView,
-#     product_list_view,
-#     ProductDetailView,
-#     ProductDetailSlugView,
-#     product_detail_view,
-#     ProductFeaturedListView,
-#     ProductFeaturedDetailView,
-#     )
+from carts.views import cart_home
 
 from .views import home_page, about_page, contact_page, login_page, register_page
 
@@ -22,17 +14,11 @@ urlpatterns = [
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
     path('login/', login_page, name='login'),
+    path('cart/', cart_home, name='cart'),
     path('register/', register_page, name='register'),
     path('bootstrap/', TemplateView.as_view(template_name='bootstrap/example.html')),
     path('products/', include("products.urls", namespace='products')),
     path('search/', include("search.urls", namespace='search')),
-    # path('featured/', ProductFeaturedListView.as_view()),
-    # path('featured/<pk>/', ProductFeaturedDetailView.as_view()),
-    # path('products/', ProductListView.as_view()),
-    # path('products-fbv/', product_list_view),
-    # # path('products/<pk>/', ProductDetailView.as_view()),
-    # path('products/<slug>/', ProductDetailSlugView.as_view()),
-    # path('products-fbv/<pk>/', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 
