89cab82bc00d82756e5e4966f98fea133bad0c40
007vict <super_vg@bk.ru>
2020-05-11 14:24:16 +0300

    create search form

--- search/urls.py
+++ search/urls.py
@@ -3,10 +3,10 @@ from .views import (
     SearchProductView,
     )
 
-app_name = 'products'
+app_name = 'search'
 
 urlpatterns = [
-    path('', SearchProductView.as_view(), name='list'),
+    path('', SearchProductView.as_view(), name='query'),
     ]
 
 
