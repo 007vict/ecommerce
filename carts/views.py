34338afbf11531dd3c5dfdf5aaec81428e6fa5e0
007vict <super_vg@bk.ru>
2020-05-12 14:16:47 +0300

    update cart

--- carts/views.py
+++ carts/views.py
@@ -1,25 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from products.models import Product
 from .models import Cart
 
 
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('Cart ID exists')
-    return cart_obj
-
 def cart_home(request):
-    request.session['cart_id'] = '12'
-    cart_id = request.session.get('cart_id', None)
-    if cart_id is None:
-        cart_obj = cart_create()
-        request.session['cart_id'] = cart_obj.id
-    else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            print('Cart ID exists')
-            cart_obj = qs.first()
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, 'carts/home.html', {})
+
+def cart_update(request):
+    product_id = 1
+    product_obj = Product.objects.get(id=product_id)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
+    # return redirect(product_obj.get_absolute_url())
+    return redirect('cart:home')
